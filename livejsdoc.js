41324133ad12c6637480e0e49ba5a2285fc8e995
Krisztian Balazs Szaniszlo <krisztian.balazs.szaniszlo@sap.com>
2015-07-29 12:29:22 +0200

    Added new option to generate JSDoc for multiple cordova plugins at once.

--- livejsdoc.js
+++ livejsdoc.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 // Import modules.
-console.log("LiveJSDoc v0.1.2\n");
+console.log("LiveJSDoc v0.2.0\n");
 var fs = require("fs"),
     util = require("util"),
     path = require("path"),
@@ -10,198 +10,243 @@ var fs = require("fs"),
 
 // Print help, if asked.
 function help() {
-	console.log("Monitors all JS files within a directory and it's subdirectories and regenerates the JSDoc when a file is edited/added/removed. Requires the 'jsdoc' command to be available.\n");
-	console.log("\tlivejsdoc <opts>\n");
-	console.log("Options:");
-	console.log("\t-h|--help|/?                Print this message and quit.");
-	console.log("\t-i|--input <input_dir>      The input directory in which to look for JS files. Cannot be used together with -c or --cordova");
-	console.log("\t-c|--cordova                The current directory is searched for an src/main/resources/www subdirectory and if it exists that is set as the input directory.");
-	console.log("\t                            Can be used for documenting cordova plugins. Equivalent to: livejsdoc -i ./src/main/resources/www -o ./target/jsdoc");
-	console.log("\t-o|--output <output_dir>    The output directory to put the generated JSDoc into. If -c is declared then it defaults to ./target/jsdoc");
-	console.log("\t-e|--exclude <files>        List of files to exclude. Each file can be a relative path within the input directory. Specify this as the last argument.");
-	console.log("");
+    console.log("Monitors all JS files within a directory and it's subdirectories and regenerates the JSDoc when a file is edited/added/removed. Requires the 'jsdoc' command to be available.\n");
+    console.log("\tlivejsdoc <opts>\n");
+    console.log("Options:");
+    console.log("\t-h|--help|/?                Print this message and quit.");
+    console.log("\t-i|--input <input_dir>      The input directory in which to look for JS files. Cannot be used together with -c or --cordova");
+    console.log("\t-c|--cordova                The current directory is searched for an src/main/resources/www subdirectory and if it exists that is set as the input directory.");
+    console.log("\t                            Can be used for documenting cordova plugins. Equivalent to: livejsdoc -i ./src/main/resources/www -o ./target/jsdoc");
+    console.log("\t-c2|--cordova-dir           Generates a combined JSDoc of cordova plugins found within the current directory. Works as if documentations were generated ");
+    console.log("\t                            using 'livejsdoc -c' and then merged into one.");
+    console.log("\t-o|--output <output_dir>    The output directory to put the generated JSDoc into. If -c or -c2 is declared then it defaults to ./target/jsdoc");
+    console.log("\t-e|--exclude <files>        List of files to exclude. Each file can be a relative path within the input directory. Specify this as the last argument.");
+    console.log("");
 }
 
 var arg_1 = process.argv[2];
 if (arg_1 == "-h" || arg_1 == "--help" || arg_1 == "/?") {
-	help();
-	process.exit(0);
+    help();
+    process.exit(0);
 }
 
 // Parse arguments.
-var in_dir, out_dir, exclude_files = [];
+var in_dirs = [],
+    out_dir, exclude_files = [];
 var within_arg;
 for (var i = 1; i < process.argv.length; i++) {
-	var arg = process.argv[i];
-	
-	// Process the argument.
-	switch (arg) {
-		case "-i":
-		case "--input":
-			if (within_arg)
-				break;
-			within_arg = "input";
-			continue;
-		case "-o":
-		case "--output":
-			if (within_arg)
-				break;
-			within_arg = "output";
-			continue;
-		case "-c":
-		case "--cordova":
-			if (within_arg)
-				break;
-			else if (in_dir)
-				console.log("Omitting " + arg + " as input directory has already been specified.");
-			else if (out_dir)
-				console.log("Omitting " + arg + " as output directory has already been specified.");
-			else {
-				in_dir = path.resolve("./src/main/resources/www");
-				if (!fs.existsSync(in_dir)) {
-					console.log("[***] JS source directory of cordova plugin does not exist: " + in_dir);
-					process.exit(1);
-				}
-				out_dir = path.resolve("./target/jsdoc");
-			}
-			continue;
-		case "-e":
-		case "--exclude":
-			if (within_arg)
-				break;
-			if (!in_dir || !out_dir) {
-				console.log("[***] Exclude must be the last argument, if specified.");
-				process.exit(1);
-			}
-			within_arg = "exclude";
-			continue;
-	}
-	
-	if (within_arg == "input") {
-		in_dir = path.resolve(arg);
-		if (!fs.existsSync(in_dir)) {
-			console.log("[***] JS source directory does not exist: " + in_dir);
-			process.exit(1);
-		}
-		within_arg = undefined;
-	} else if (within_arg == "output") {
-		out_dir = path.resolve(arg);
-		within_arg = undefined;
-	} else if (within_arg == "exclude")
-		exclude_files.push(path.resolve(in_dir + "/" + arg));
+    var arg = process.argv[i];
+
+    // Process the argument.
+    switch (arg) {
+        case "-i":
+        case "--input":
+            if (within_arg)
+                break;
+            within_arg = "input";
+            continue;
+        case "-o":
+        case "--output":
+            if (within_arg)
+                break;
+            within_arg = "output";
+            continue;
+        case "-c":
+        case "--cordova":
+            if (within_arg)
+                break;
+            else if (in_dirs.length > 0)
+                console.log("Omitting " + arg + " as input directory has already been specified.");
+            else if (out_dir)
+                console.log("Omitting " + arg + " as output directory has already been specified.");
+            else {
+                var in_dir = path.resolve("./src/main/resources/www");
+                if (!fs.existsSync(in_dir)) {
+                    console.log("[***] JS source directory of cordova plugin does not exist: " + in_dir);
+                    process.exit(1);
+                }
+                in_dirs.push(in_dir);
+                out_dir = path.resolve("./target/jsdoc");
+            }
+            continue;
+        case "-c2":
+        case "--cordova-dir":
+            if (within_arg)
+                break;
+            else if (in_dirs.length > 0)
+                console.log("Omitting " + arg + " as input directory has already been specified.");
+            else if (out_dir)
+                console.log("Omitting " + arg + " as output directory has already been specified.");
+            else {
+                (function(arg) {
+                    fs.readdirSync(path.resolve(".")).forEach(function(file, index) {
+                        var curPath = path.resolve("./" + file + "/src/main/resources/www");
+                        if (fs.existsSync(curPath) && fs.lstatSync(curPath).isDirectory())
+                            in_dirs.push(curPath);
+                    });
+                })(arg);
+                out_dir = path.resolve("./target/jsdoc");
+            }
+            continue;
+        case "-e":
+        case "--exclude":
+            if (within_arg)
+                break;
+            if (in_dirs.length === 0 || !out_dir) {
+                console.log("[***] Exclude must be the last argument, if specified.");
+                process.exit(1);
+            }
+            within_arg = "exclude";
+            continue;
+    }
+
+    if (within_arg == "input") {
+        var in_dir = path.resolve(arg);
+        if (!fs.existsSync(in_dir)) {
+            console.log("[***] JS source directory does not exist: " + in_dir);
+            process.exit(1);
+        }
+        in_dirs.push(in_dir);
+        within_arg = undefined;
+    } else if (within_arg == "output") {
+        out_dir = path.resolve(arg);
+        within_arg = undefined;
+    } else if (within_arg == "exclude")
+        in_dirs.forEach(function(in_dir) {
+            exclude_files.push(path.resolve(in_dir + "/" + arg));
+        });
 }
 
 // Check if we've got everything.
-if (!in_dir) {
-	help();
-	console.log("[***] Input directory was not specified.")
-	process.exit(1);
+if (in_dirs.length === 0) {
+    help();
+    console.log("[***] Input directory was not specified.");
+    process.exit(1);
 } else if (!out_dir) {
-	help();
-	console.log("[***] Output directory was not specified.")
-	process.exit(1);
+    help();
+    console.log("[***] Output directory was not specified.");
+    process.exit(1);
 }
 
-console.log("Input directory: " + in_dir);
+in_dirs.forEach(function(in_dir) {
+    console.log("Input directory: " + in_dir);
+});
 console.log("Output directory: " + out_dir);
 if (exclude_files.length > 0)
-	console.log("Excluding files: \n\t" + exclude_files.join("\n\t"));
+    console.log("Excluding files: \n\t" + exclude_files.join("\n\t"));
 
 // Walks through a directory recursively and reports the set of JS files via the 'done' callback.
 function isExcluded(file) {
-	if (exclude_files.indexOf(file) >= 0)
-		return true;
+    if (exclude_files.indexOf(file) >= 0)
+        return true;
 
-	for (var i = 0; i < exclude_files.length; i++)
-		if (path.resolve(in_dir + "/" + exclude_files[i]) == file)
-			return true;
-
-	return false;
+    return exclude_files.some(function(exclude_file) {
+        return in_dirs.some(function(in_dir) {
+            if (path.resolve(in_dir + "/" + exclude_file) == file)
+                return true;
+        });
+    });
 }
 
 function walk(dir, done) {
-	var results = [];
-	fs.readdir(dir, function(err, list) {
-		if (err)
-			return done(err);
-		var i = 0;
-		(function next() {
-			var file = list[i++];
-			if (!file)
-				return done(null, results);
-			file = path.resolve(dir + "/" + file);
-			fs.stat(file, function(err, stat) {
-				if (stat && stat.isDirectory()) {
-					walk(file, function(err, res) {
-						results = results.concat(res);
-						next();
-					});
-				} else if (/.js$/.test(file) && !isExcluded(file)) {
-					results.push(file);
-					next();
-				} else {
-					next();
-				}
-			});
-		})();
-	});
+    var results = [];
+    fs.readdir(dir, function(err, list) {
+        if (err)
+            return done(err);
+        var i = 0;
+        (function next() {
+            var file = list[i++];
+            if (!file)
+                return done(null, results);
+            file = path.resolve(dir + "/" + file);
+            fs.stat(file, function(err, stat) {
+                if (stat && stat.isDirectory()) {
+                    walk(file, function(err, res) {
+                        results = results.concat(res);
+                        next();
+                    });
+                } else if (/.js$/.test(file) && !isExcluded(file)) {
+                    results.push(file);
+                    next();
+                } else {
+                    next();
+                }
+            });
+        })();
+    });
 }
 
 // Remove directory recursively.
 function deleteFolderRecursive(path) {
-	if (fs.existsSync(path)) {
-		fs.readdirSync(path).forEach(function(file, index) {
-			var curPath = path + "/" + file;
-			if (fs.lstatSync(curPath).isDirectory())
-				deleteFolderRecursive(curPath);
-			else
-				fs.unlinkSync(curPath);
-		});
-		fs.rmdirSync(path);
-	}
-};
+    if (fs.existsSync(path)) {
+        fs.readdirSync(path).forEach(function(file, index) {
+            var curPath = path + "/" + file;
+            if (fs.lstatSync(curPath).isDirectory())
+                deleteFolderRecursive(curPath);
+            else
+                fs.unlinkSync(curPath);
+        });
+        fs.rmdirSync(path);
+    }
+}
 
 // Manages public/private mode.
 var publicMode = true;
+
 function getModeStr() {
-	return publicMode ? "public" : "private";
+    return publicMode ? "public" : "private";
 }
 
 // Performs generation using the files in the source directory.
 function generate() {
-	walk(in_dir, function(err, results) {
-		if (err)
-			return;
+    var files = [],
+        quotedFiles = [],
+        n = in_dirs.length;
 
-		var files = [];
-		for (var i = 0; i < results.length; i++)
-			files.push("\"" + results[i] + "\"");
+    function done() {
+        if (--n > 0)
+            return;
 
-		console.log("[***] Generating JSDoc to output (mode: " + getModeStr() + "). Files: \n\t" + results.join("\n\t"));
-		exec("jsdoc " + (!publicMode ? "-p " : "") + "-d \"" + out_dir + "\" " + files.join(" "), function(err, out, code) {
-			if (err) {
-				console.log("[***] Error executing JSDoc. Probably there was a syntax error. Documentation was not regenerated. If the problem persists make sure you have installed JSDoc properly (using 'npm install -g jsdoc')");
-				console.log("[***] Exit code: " + code);
-				console.log("[***] Error description: " + err);
-			}
-		});
-	});
+        console.log("[***] Generating JSDoc to output (mode: " + getModeStr() + "). Files: \n\t" + files.join("\n\t"));
+        exec("jsdoc " + (!publicMode ? "-p " : "") + "-d \"" + out_dir + "\" " + quotedFiles.join(" "), function(err, out, code) {
+            if (err) {
+                console.log("[***] Error executing JSDoc. Probably there was a syntax error. Documentation was not regenerated. If the problem persists make sure you have installed JSDoc properly (using 'npm install -g jsdoc')");
+                console.log("[***] Exit code: " + code);
+                console.log("[***] Error description: " + err);
+            }
+        });
+    }
+
+    in_dirs.forEach(function(in_dir) {
+        walk(in_dir, function(err, results) {
+            if (err)
+                return;
+
+            for (var i = 0; i < results.length; i++) {
+                files.push(results[i]);
+                quotedFiles.push("\"" + results[i] + "\"");
+            }
+
+            done();
+        });
+    });
 }
 
 generate();
 
-// Initialize watching the source directory.
-fs.watch(in_dir, (function() {
-	var h;
-
-	return function(evt, dirname) {
-		if (!h)
-			h = setTimeout(function() {
-				generate();
-				h = undefined;
-			}, 250);
-	};
-})());
+// Initialize watching the source directories.
+(function() {
+    var h;
+    in_dirs.forEach(function(in_dir) {
+        fs.watch(in_dir, function(evt, dirname) {
+            if (!h)
+                h = setTimeout(function() {
+                    generate();
+                    h = undefined;
+                }, 250);
+        });
+    });
+})();
 
 // Print some info.
 console.log("Commands:");
@@ -216,21 +261,21 @@ stdin.setRawMode(true);
 stdin.resume();
 stdin.setEncoding("utf-8");
 stdin.on("data", function(key) {
-	if (key === "\u0003" || key == "q" || key == "Q")
-		process.exit();
-	else if (key == "p" || key == "P") {
-		publicMode = !publicMode;
-		console.log("[***] Switching to " + getModeStr() + " mode.");
-		generate();
-	} else if (key == "c" || key == "C") {
-		console.log("[***] Clearing output directory: " + out_dir);
-		deleteFolderRecursive(out_dir);
-		generate();
-	} else if (key == "o" || key == "O") {
-		var indexHtml = path.resolve(out_dir + "/index.html");
-		if (!fs.existsSync(indexHtml))
-			console.log("[***] Unable to open documentation as it has not yet been generated.");
-		else
-			open(indexHtml);
-	}
+    if (key === "\u0003" || key == "q" || key == "Q")
+        process.exit();
+    else if (key == "p" || key == "P") {
+        publicMode = !publicMode;
+        console.log("[***] Switching to " + getModeStr() + " mode.");
+        generate();
+    } else if (key == "c" || key == "C") {
+        console.log("[***] Clearing output directory: " + out_dir);
+        deleteFolderRecursive(out_dir);
+        generate();
+    } else if (key == "o" || key == "O") {
+        var indexHtml = path.resolve(out_dir + "/index.html");
+        if (!fs.existsSync(indexHtml))
+            console.log("[***] Unable to open documentation as it has not yet been generated.");
+        else
+            open(indexHtml);
+    }
 });
